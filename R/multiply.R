975e74c15107913a96000662a597ec449aadfd9c
Weijia <wjxiong5633@gmail.com>
2025-03-24 14:12:49 +0800

    add another fun

---
+++ R/multiply.R
@@ -0,0 +1,13 @@
+#' multiply 100 to the given number
+#'
+#' @param x a number, float or int
+#'
+#' @return a number
+#' @export
+#'
+#' @examples
+#' multiply100(5)
+#' multiply100(-90)
+multiply100=function(x){
+  return(x*100)
+}
